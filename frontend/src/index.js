831a95e69c00d56c07511d26847ef6c414ebb981
soumytiwari <tiwarisoumya111@gmail.com>
2024-08-10 23:16:50 +0530

    setup for alert provider by react-alert

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import {Provider} from "react-redux"
+import { transitions, positions, Provider as AlertProvider } from "react-alert"
+import AlertTemplate from 'react-alert-template-basic'
 import store from "./store";
+
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // using the provider, we are connecting react from redux
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AlertProvider>
   </Provider>
 );
 
