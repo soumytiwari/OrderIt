c47948ca9005eff1a6590f4db23dda28a72fbdf6
soumytiwari <tiwarisoumya111@gmail.com>
2024-08-11 02:04:02 +0530

    created new signup and login routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Header from "./components/layouts/Header";
 import Home from "./components/layouts/Home";
 import Footer from "./components/layouts/Footer";
 import Menu from "./components/layouts/Menu";
+import Login from './components/users/Login'
+import Register from './components/users/Register'
 import Cart from "./components/cart/Cart";
 // import PageNotFound from "./404Page"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
@@ -19,7 +21,9 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/eats/stores/:id/menus" element={<Menu />}/>
-            <Route path="/Cart" element={<Cart />}/>
+            <Route path='/users/login' element={<Login />}/>
+            <Route path="users/signup" element={<Register />}/>
+            {/* <Route path="/Cart" element={<Cart />}/> */}
             {/* <Route path="*" element={<PageNotFound />}/> */}
           </Routes>
         </div>
