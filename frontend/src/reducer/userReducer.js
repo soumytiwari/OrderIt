8910e283352617985775897961918072748c0436
soumytiwari <tiwarisoumya111@gmail.com>
2024-08-14 01:29:36 +0530

    created reducer for the updateProfile, step 3. in 1 file, 2 reducers, yess

--- frontend/src/reducer/userReducer.js
+++ frontend/src/reducer/userReducer.js
@@ -13,6 +13,10 @@ import {
   REGISTER_USER_FAIL,
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
+  UPDATE_PROFILE_FAIL,
+  UPDATE_PROFILE_REQUEST,
+  UPDATE_PROFILE_RESET,
+  UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstant";
 
 export const authReducer = (
@@ -79,3 +83,41 @@ export const authReducer = (
       return state;
   }
 };
+
+export const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case UPDATE_PROFILE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      }
+
+    case UPDATE_PROFILE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isUpdated: action.payload,
+      }
+
+    case UPDATE_PROFILE_RESET:
+      return {
+        ...state,
+        isUpdated: false,
+      }
+
+    case UPDATE_PROFILE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      }
+  
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+  
+    default:
+      return state;
+  }
+}
