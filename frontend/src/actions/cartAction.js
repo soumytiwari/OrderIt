50260f542364b36f33eab4adf62486eee50f5b3a
soumytiwari <tiwarisoumya111@gmail.com>
2024-08-17 14:51:01 +0530

    modified to fix error

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -1,5 +1,12 @@
 import axios from "axios";
-import { ADD_TO_CART, CLEAR_CART, FETCH_CART, REMOVE_ITEM_CART, UPDATE_CART_ITEM } from "../constants/cartConstant";
+import {
+  ADD_TO_CART,
+  CLEAR_CART,
+  FETCH_CART,
+  REMOVE_ITEM_CART,
+  UPDATE_CART_ITEM,
+} from "../constants/cartConstant";
+
 
 export const fetchCartItems = (alert) => async (dispatch) => {
   try {
@@ -20,96 +27,89 @@ export const fetchCartItems = (alert) => async (dispatch) => {
 export const addItemToCart =
   (foodItemId, restaurant, quantity, alert) => async (dispatch, getState) => {
     try {
-        // if the user is logged, then only add to the cart
-        const { user } = getState().auth                //  return the current store tree
-        const response = await axios.post(
-            "/api/v1/eats/cart/add-to-cart",
-            {
-                userId: user._id,
-                foodItemId,
-                restaurantId: restaurant,
-                quantity,
-            }
-        )
+      // if the user is logged, then only add to the cart
+      const { user } = getState().auth; //  return the current store tree
+      const response = await axios.post("/api/v1/eats/cart/add-to-cart", {
+        userId: user._id,
+        foodItemId,
+        restaurantId: restaurant,
+        quantity,
+      });
 
-        alert.success("Item added to cart", response.data.cart)
-        dispatch({
-            type: ADD_TO_CART,
-            payload: response.data.cart,
-        })
+      alert.success("Item added to cart", response.data.cart);
+      dispatch({
+        type: ADD_TO_CART,
+        payload: response.data.cart,
+      });
     } catch (error) {
-        alert.error(error.response ? error.response.data.message : error.message)
+      alert.error(error.response ? error.response.data.message : error.message);
     }
   };
 
 // Update cart Item quantity
-export const updateCartQuantity = (foodItemId, quantity, alert) => async(dispatch, getState) => {
+export const updateCartQuantity =
+  (foodItemId, quantity, alert) => async (dispatch, getState) => {
     try {
-        const { user } = getState().auth;
+      const { user } = getState().auth;
 
-        // converting the food item id to normal id
-        if(typeof foodItemId === "object") {
-            foodItemId = foodItemId._id
-        }
+      // converting the food item id to normal id
+      if (typeof foodItemId === "object") {
+        foodItemId = foodItemId._id;
+      }
 
-        const response = await axios.post(
-            "/api/v1/eats/cart/update-cart-item",
-            {
-                userId: user._id,
-                foodItemId: foodItemId,
-                quantity,
-            }
-        )
+      const response = await axios.post("/api/v1/eats/cart/update-cart-item", {
+        userId: user._id,
+        foodItemId: foodItemId,
+        quantity,
+      });
 
-        dispatch({
-            type: UPDATE_CART_ITEM,
-            payload: response.data.cart,
-        })
+      dispatch({
+        type: UPDATE_CART_ITEM,
+        payload: response.data.cart,
+      });
     } catch (error) {
-        alert.error(error.response ? error.response.data.message : error.message)
+      alert.error(error.response ? error.response.data.message : error.message);
     }
-}
+  };
 
 // Remove items from cart
-export const removeItemFromCart = (foodItemId, alert) => async(dispatch, getState) => {
+export const removeItemFromCart =
+  (foodItemId, alert) => async (dispatch, getState) => {
     try {
-        const { user } = getState().auth
-        if(typeof foodItemId === "object") {
-            foodItemId = foodItemId._id
+      const { user } = getState().auth;
+      if (typeof foodItemId === "object") {
+        foodItemId = foodItemId._id;
+      }
+
+      const response = await axios.delete(
+        "/api/v1/eats/cart/delete-cart-item",
+        {
+          data: { userId: user._id, foodItemId },
         }
+      );
 
-        const response = await axios.delete(
-            "/api/v1/eats/cart/delete-cart-item",
-            {
-                data: { userId: user._id, foodItemId }
-            }
-        )
-
-        dispatch({
-            type: REMOVE_ITEM_CART,
-            payload: response.data,
-        })
+      dispatch({
+        type: REMOVE_ITEM_CART,
+        payload: response.data,
+      });
     } catch (error) {
-        alert.error(error.response ? error.response.data.message : error.message)
+      alert.error(error.response ? error.response.data.message : error.message);
     }
-}
+  };
 
 // clear cart
-export const deleteCart = (alert) => async(dispatch, getState) => {
-    try {
-        const { user } = getState().auth
-        const response = await axios.delete(
-            "/api/v1/eats/cart/delete-cart",
-            {
-                data: { userId: user._id }
-            }
-        )
+export const deleteCart = (alert) => async (dispatch, getState) => {
+  try {
+    const { user } = getState().auth;
+    const response = await axios.delete("/api/v1/eats/cart/delete-cart", {
+      data: { userId: user._id },
+    });
 
-        dispatch({
-            type: CLEAR_CART,
-            payload: response.data,
-        })
-    } catch (error) {
-        alert.error(error.response ? error.response.data.message : error.message)
-    }
-}
+    dispatch({
+      type: CLEAR_CART,
+      payload: response.data,
+    });
+  } catch (error) {
+    alert.error(error.response ? error.response.data.message : error.message);
+  }
+};
