ae7a5f0cc7f717b5885ef32cbf5d93df00eba755
soumytiwari <tiwarisoumya111@gmail.com>
2024-08-18 14:19:03 +0530

    modified

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -25,6 +25,7 @@ import {
   UPDATE_PROFILE_REQUEST,
   UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstant";
+import { CLEAR_CART } from "../constants/cartConstant";
 
 // Login
 export const login = (email, password) => async (dispatch) => {
@@ -137,6 +138,9 @@ export const logout = () => async (dispatch) => {
     dispatch({
       type: LOGOUT_SUCCESS,
     });
+    dispatch({
+      type: CLEAR_CART,                       //  clear cart when log out
+    })
   } catch (error) {
     dispatch({
       type: LOGOUT_FAIL,
